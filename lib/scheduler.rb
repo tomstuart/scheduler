52cf7fce78aa609877a670e4700b687c08b9b34e
Tom Stuart <tom@codon.com>
2013-12-21 22:01:04 +0000

    Implement Scheduler class

---
+++ lib/scheduler.rb
@@ -0,0 +1,19 @@
+class Scheduler
+  attr_accessor :start_time, :end_time
+
+  def initialize(start_time, end_time)
+    self.start_time = start_time
+    self.end_time = end_time
+  end
+
+  def two_times
+    calendar = Calendar.new(start_time, end_time)
+    all_periods = calendar.periods
+
+    first_half_periods, second_half_periods = all_periods.split_into(2)
+    first_time = first_half_periods.random_time
+    second_time = second_half_periods.random_time
+
+    [first_time, second_time]
+  end
+end
---
+++ spec/scheduler_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+require 'scheduler'
+
+describe Scheduler do
+  describe 'accessors' do
+    let(:start_time) { double('start time') }
+    let(:end_time) { double('end time') }
+    subject { Scheduler.new(start_time, end_time) }
+
+    its(:start_time) { should == start_time }
+    its(:end_time) { should == end_time }
+  end
+
+  describe '#two_times' do
+    let(:start_time) { double('start time') }
+    let(:end_time) { double('end time') }
+    let(:calendar) { double('calendar', periods: periods) }
+    let(:periods) { double('periods', split_into: [first_half_periods, second_half_periods]) }
+    let(:first_half_periods) { double('first half periods', random_time: first_time) }
+    let(:second_half_periods) { double('second half periods', random_time: second_time) }
+    let(:first_time) { double('first time') }
+    let(:second_time) { double('second time') }
+    subject { Scheduler.new(start_time, end_time) }
+
+    before(:each) do
+      stub_const('Calendar', double('fake Calendar class', new: calendar))
+    end
+
+    it 'should make a new calendar with the right start and end times' do
+      Calendar.should_receive(:new).with(start_time, end_time)
+      subject.two_times
+    end
+
+    it 'should ask the calendar for its periods' do
+      calendar.should_receive(:periods).with(no_args)
+      subject.two_times
+    end
+
+    it 'should split the periods into two halves' do
+      periods.should_receive(:split_into).with(2)
+      subject.two_times
+    end
+
+    it 'should ask each half for a random time' do
+      first_half_periods.should_receive(:random_time).with(no_args)
+      second_half_periods.should_receive(:random_time).with(no_args)
+      subject.two_times
+    end
+
+    it 'should return the two times from the two halves' do
+      subject.two_times.should == [first_time, second_time]
+    end
+  end
+end
