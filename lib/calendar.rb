eb8e898fcdddd638d96a4af7a24f18f9c598f86c
Tom Stuart <tom@codon.com>
2013-12-21 22:40:03 +0000

    Implement Calendar#periods

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -1,4 +1,28 @@
+require 'active_support/core_ext'
+
 class Calendar
+  START_HOUR = 9
+  END_HOUR = 17
+
+  attr_accessor :start_time, :end_time
+
   def initialize(start_time, end_time)
+    self.start_time = start_time
+    self.end_time = end_time
+  end
+
+  def periods
+    periods = []
+
+    period_start_time = start_time.change(hour: START_HOUR)
+    period_start_time += 1.day until start_time <= period_start_time
+
+    until period_start_time > end_time
+      period_end_time = period_start_time.change(hour: END_HOUR)
+      periods << Period.new(period_start_time, period_end_time) unless period_start_time.saturday? || period_start_time.sunday?
+      period_start_time += 1.day
+    end
+
+    PeriodGroup.new(periods)
   end
 end
--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -11,4 +11,64 @@ describe Calendar do
       -> { Calendar.new(start_time, end_time) }.should_not raise_error
     end
   end
+
+  describe '#periods' do
+    let(:start_time) { DateTime.new(2013, 12, 2, 0, 0) }
+    let(:end_time) { DateTime.new(2013, 12, 8, 23, 59, 59) }
+
+    let(:monday_period)     { double('Monday period') }
+    let(:tuesday_period)    { double('Tuesday period') }
+    let(:wednesday_period)  { double('Wednesday period') }
+    let(:thursday_period)   { double('Thursday period') }
+    let(:friday_period)     { double('Friday period') }
+
+    let(:periods) { double('periods') }
+
+    subject(:calendar) { Calendar.new(start_time, end_time) }
+
+    before(:each) do
+      stub_const('Period', 'fake Period class')
+      Period.stub(:new).with(DateTime.new(2013, 12, 2, 9, 0), DateTime.new(2013, 12, 2, 17, 0)).and_return(monday_period)
+      Period.stub(:new).with(DateTime.new(2013, 12, 3, 9, 0), DateTime.new(2013, 12, 3, 17, 0)).and_return(tuesday_period)
+      Period.stub(:new).with(DateTime.new(2013, 12, 4, 9, 0), DateTime.new(2013, 12, 4, 17, 0)).and_return(wednesday_period)
+      Period.stub(:new).with(DateTime.new(2013, 12, 5, 9, 0), DateTime.new(2013, 12, 5, 17, 0)).and_return(thursday_period)
+      Period.stub(:new).with(DateTime.new(2013, 12, 6, 9, 0), DateTime.new(2013, 12, 6, 17, 0)).and_return(friday_period)
+
+      stub_const('PeriodGroup', double('fake PeriodGroup class', new: periods))
+    end
+
+    it 'should create the Monday period' do
+      Period.should_receive(:new).with(DateTime.new(2013, 12, 2, 9, 0), DateTime.new(2013, 12, 2, 17, 0))
+      calendar.periods
+    end
+
+    it 'should create the Tuesday period' do
+      Period.should_receive(:new).with(DateTime.new(2013, 12, 3, 9, 0), DateTime.new(2013, 12, 3, 17, 0))
+      calendar.periods
+    end
+
+    it 'should create the Wednesday period' do
+      Period.should_receive(:new).with(DateTime.new(2013, 12, 4, 9, 0), DateTime.new(2013, 12, 4, 17, 0))
+      calendar.periods
+    end
+
+    it 'should create the Thursday period' do
+      Period.should_receive(:new).with(DateTime.new(2013, 12, 5, 9, 0), DateTime.new(2013, 12, 5, 17, 0))
+      calendar.periods
+    end
+
+    it 'should create the Friday period' do
+      Period.should_receive(:new).with(DateTime.new(2013, 12, 6, 9, 0), DateTime.new(2013, 12, 6, 17, 0))
+      calendar.periods
+    end
+
+    it 'should create a group of periods' do
+      PeriodGroup.should_receive(:new).with([monday_period, tuesday_period, wednesday_period, thursday_period, friday_period])
+      calendar.periods
+    end
+
+    it 'should return the group of periods' do
+      calendar.periods.should == periods
+    end
+  end
 end
