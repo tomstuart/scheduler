9fda588227f63698f0d35681635fdf0afa90132d
Tom Stuart <tom@codon.com>
2013-12-21 22:39:35 +0000

    Use DateTime instances instead of Time
    Because that's what ActiveSupport expects us to use.

--- example.rb
+++ example.rb
@@ -1,7 +1,7 @@
 require 'scheduler'
 
-start_time = Time.local(2013, 12, 2, 0, 0)
-end_time = Time.local(2013, 12, 8, 23, 59, 59)
+start_time = DateTime.new(2013, 12, 2, 0, 0)
+end_time = DateTime.new(2013, 12, 8, 23, 59, 59)
 scheduler = Scheduler.new(start_time, end_time)
 first_time, second_time = scheduler.two_times
 
