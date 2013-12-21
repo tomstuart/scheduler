7a4580b7b5f890c10c24d820538ef9c16a612209
Tom Stuart <tom@codon.com>
2013-12-21 22:00:44 +0000

    Add example script that uses the scheduler

---
+++ example.rb
@@ -0,0 +1,8 @@
+require 'scheduler'
+
+start_time = Time.local(2013, 12, 2, 0, 0)
+end_time = Time.local(2013, 12, 8, 23, 59, 59)
+scheduler = Scheduler.new(start_time, end_time)
+first_time, second_time = scheduler.two_times
+
+puts "First time is #{first_time}, second time is #{second_time}"
