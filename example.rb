86b7d2acfd363b38f1e3ab4ce7f13a60f3e6f427
Tom Stuart <tom@codon.com>
2013-12-22 12:23:59 +0000

    Update acceptance test to ask for more times

--- example.rb
+++ example.rb
@@ -4,10 +4,10 @@ require 'date'
 start_time = Time.local(2013, 12, 2, 0, 0)
 end_time = Time.local(2013, 12, 8, 23, 59, 59)
 scheduler = Scheduler.new(start_time, end_time)
-first_time, second_time = scheduler.pick_times(2)
+times = scheduler.pick_times(5)
 
-raise 'not times' unless first_time.is_a?(Time) && second_time.is_a?(Time)
-raise 'wrong times' unless first_time.between?(start_time, end_time) && second_time.between?(start_time, end_time)
-raise 'same times' unless first_time != second_time
+raise 'not times' unless times.all? { |time| time.is_a?(Time) }
+raise 'wrong times' unless times.all? { |time| time.between?(start_time, end_time) }
+raise 'same times' unless times.uniq.length == times.length
 
-puts "First time is #{first_time}, second time is #{second_time}"
+puts "Times are #{times.join(', ')}"
