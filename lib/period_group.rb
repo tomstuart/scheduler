06ec2ddf4dbf6c4eabb1d4587a3ed5f627d0b028
Tom Stuart <tom@codon.com>
2013-12-22 14:06:47 +0000

    Extract ideal_piece_size local

--- lib/period_group.rb
+++ lib/period_group.rb
@@ -1,7 +1,9 @@
 class PeriodGroup < Struct.new(:periods)
   def split_into(number_of_pieces)
+    ideal_piece_size = periods.length.to_f / number_of_pieces
+
     (0..number_of_pieces).
-      map { |piece_number| (piece_number * (periods.length.to_f / number_of_pieces)).round }.
+      map { |piece_number| (piece_number * ideal_piece_size).round }.
       each_cons(2).
       map { |piece_offset, next_piece_offset| PeriodGroup.new(periods.slice(piece_offset...next_piece_offset)) }
   end
