1e1d26fd4b81440ab83296779ece763f3d82b190
Tom Stuart <tom@codon.com>
2013-12-22 10:56:19 +0000

    Make acceptance test more discriminating

--- example.rb
+++ example.rb
@@ -6,4 +6,8 @@ end_time = DateTime.new(2013, 12, 8, 23, 59, 59)
 scheduler = Scheduler.new(start_time, end_time)
 first_time, second_time = scheduler.two_times
 
+raise 'not times' unless first_time.is_a?(DateTime) && second_time.is_a?(DateTime)
+raise 'wrong times' unless first_time.between?(start_time, end_time) && second_time.between?(start_time, end_time)
+raise 'same times' unless first_time != second_time
+
 puts "First time is #{first_time}, second time is #{second_time}"
