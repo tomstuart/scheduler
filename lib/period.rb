69cfbd2b958117412e0c9d79dff67afdbc90d9cd
Tom Stuart <tom@codon.com>
2013-12-22 11:51:57 +0000

    Implement Period#end_time

--- lib/period.rb
+++ lib/period.rb
@@ -1,4 +1,7 @@
 class Period
+  attr_accessor :end_time
+
   def initialize(start_time, end_time)
+    self.end_time = end_time
   end
 end
--- spec/period_spec.rb
+++ spec/period_spec.rb
@@ -11,4 +11,12 @@ describe Period do
       -> { Period.new(start_time, end_time) }.should_not raise_error
     end
   end
+
+  describe '#end_time' do
+    let(:start_time) { double('start time') }
+    let(:end_time) { double('end time') }
+    subject { Period.new(start_time, end_time) }
+
+    its(:end_time) { should == end_time }
+  end
 end
