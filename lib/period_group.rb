e3c8d910bb3eb91d8283aeb6dd18a91805155766
Tom Stuart <tom@codon.com>
2013-12-23 09:41:19 +0000

    Use a Rational instead of a Float in PeriodGroup#split_into
    This avoids any loss of precision due to (premature) rounding.

--- lib/period_group.rb
+++ lib/period_group.rb
@@ -1,6 +1,6 @@
 class PeriodGroup < Struct.new(:periods)
   def split_into(number_of_pieces)
-    ideal_piece_size = periods.length.to_f / number_of_pieces
+    ideal_piece_size = Rational(periods.length, number_of_pieces)
 
     (0..number_of_pieces).
       map { |piece_number| (piece_number * ideal_piece_size).round }.
